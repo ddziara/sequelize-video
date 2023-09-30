d74b9a508779c3158cf828e1b199e0c5ba4c8b81
Dariusz Dziara <ddziara@poczta.onet.pl>
2023-09-30 16:43:44 +0200

    Querying, updating, destroying

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const { DataTypes } = Sequelize;
+const { DataTypes, Op } = Sequelize;
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -51,31 +51,59 @@ const User = sequelize.define(
 User.sync({ alter: true })
   .then(() => {
     // working with our updated table
-    return User.bulkCreate(
-      [
-        {
-          username: "Tomrewrewytrey",
-          age: 25,
-          password: "pizzasocker",
-        },
-        {
-          username: "M",
-          age: 31,
-          password: "12345",
-        },
-        {
-          username: "Freddiedfgfdhgh",
-        },
-      ],
-      {
-        validate: true,
-      }
-    );
+    // return User.findAll();
+    // return User.findAll({ attributes: ["username", "password"] });
+    // return User.findAll({ attributes: [["username", "myName"], ["password", "pwd"]] });
+    // return User.findAll({attributes: [[sequelize.fn('SUM', sequelize.col('age')), 'howOld']]});
+    // return User.findAll({attributes: [[sequelize.fn('AVG', sequelize.col('age')), 'howOld']]});
+    // return User.findAll({ attributes: { exclude: ["password"] } });
+    // return User.findAll({ attributes: ['username'], where: { age: 45 } });
+    // return User.findAll({ where: { age: 25, username: "pizza" } });
+    // return User.findAll({ limit: 2});
+    // return User.findAll({ order: [['age','DESC']]});
+    // return User.findAll({ attributes: ['username', [sequelize.fn('SUM', sequelize.col('age')), 'sum_age']], group: 'username'});
+    // return User.findAll({
+    //   where: {
+    //     [Op.or]: { username: "soccer", age: 45 },
+    //   },
+    // });
+    // return User.findAll({ where: {
+    //   age: {
+    //     [Op.gt]: 25
+    //   }
+    // }});
+    // return User.findAll({ where: {
+    //   age: {
+    //     [Op.or]: {
+    //       [Op.lt]: 45,
+    //       [Op.eq]: null
+    //     }
+    //   }
+    // }});
+    // return User.findAll({ where:
+    //   sequelize.where(sequelize.fn('char_length', sequelize.col('username')), 5)
+    // });
+    // return User.update({ username: "pizza" }, { where: { age: 25 } });
+    // return User.update(
+    //   { username: "Yes!" },
+    //   { where: { age: { [Op.gt]: 1 } } }
+    // );
+    // return User.destroy({ where: { username: "Yes!" } });
+    // return User.destroy({ truncate: true });
+    // return User.create({
+    //   username: 'oldy',
+    //   password: '1234',
+    //   age: 87
+    // })
+    // return User.max('age');          // utility function
+    // return User.sum('age');          // utility function
+    return User.sum("age", { where: { age: 21 } }); // utility function
   })
   .then((data) => {
-    data.forEach((element) => {
-      console.log(element.toJSON());
-    });
+    console.log(data);
+    // data.forEach((element) => {
+    //   console.log(element.toJSON());
+    // });
   })
   .catch((err) => {
     console.log(err);
